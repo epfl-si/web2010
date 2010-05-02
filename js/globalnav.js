b59ef3c9dcab6a84506d6f4293b55974dae689e3
grfavre <grfavre@7c446e51-0b01-4665-bd70-fc07983dad15>
2010-05-02 12:51:17 +0000

    header completed

--- js/globalnav.js
+++ js/globalnav.js
@@ -7,22 +7,50 @@
  * Revision: 1
  */
 $(document).ready(function(){
+    /* Navigation: big panels */
     var opened_panel = null;
-    
-    function toggle_panel(panel){
-        if (panel == opened_panel){ 
-            opened_panel.hide();
-            opened_panel = null;
-            return;
-        }
+    function hide_panel(){
+        opened_panel.hide(); 
+        opened_panel = null;
+    }
+    function show_panel(panel){
         if (opened_panel != null) opened_panel.hide();   
-        panel.show()
+        panel.show();
         opened_panel = panel;
     }
-    $('#main_menus .menu').mouseleave(function(){ opened_panel.hide(); opened_panel=null; });
-        
-    $('#public_menu_link').mouseenter(function(){ toggle_panel($('#public_panel')); });
-    $('#school_menu_link').mouseenter(function(){ toggle_panel($('#school_panel')); });
-    $('#brief_menu_link').mouseenter(function(){ toggle_panel($('#brief_panel')); });
-          
+    $('#main_menus .menu').mouseleave(function(){ hide_panel(); });
+    $('#public_menu_link').mouseenter(function(){ show_panel($('#public_panel')); });
+    $('#school_menu_link').mouseenter(function(){ show_panel($('#school_panel')); });
+    $('#brief_menu_link').mouseenter(function(){ show_panel($('#brief_panel')); });
+    
+    /* Navigation: search boxes */
+    var opened_searchbox = null;
+    function toggle_searchbox(searchbox){
+        if (opened_searchbox != null){
+            opened_searchbox.hide();
+            opened_searchbox.siblings('a').toggleClass('current');
+        }
+        searchbox.show();
+        searchbox.siblings('a').toggleClass('current');
+        opened_searchbox = searchbox;
+    }
+    $('#search_person').mouseenter(function(){ toggle_searchbox($('#search_person_form')); });
+    $('#search_place').mouseenter(function(){ toggle_searchbox($('#search_place_form')); });
+    $('#search_epfl').mouseenter(function(){ toggle_searchbox($('#search_epfl_form')); });
+    $('#search_local').mouseenter(function(){ toggle_searchbox($('#search_local_form')); });
+    toggle_searchbox($('#search_person_form'));
+    
+    /* Texts input in header (search fields) */
+    $('#header').find('input[title]').each(function() {
+        if ($(this).val() === '') $(this).val($(this).attr('title'));
+        $(this).focus(function() {
+            if ($(this).val() === $(this).attr('title')) $(this).val('').addClass('focused');
+        });
+        $(this).blur(function() {
+            if ($(this).val() === '')  $(this).val($(this).attr('title')).removeClass('focused');
+        });
+        $(this).keypress(function(e){
+            if (e.which == 13) $(this).parent('form').submit();
+        });
+    });
 });
