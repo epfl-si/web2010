2b43e98d98fe1714a75023f1508cfc00c2bbe2da
grfavre <grfavre@7c446e51-0b01-4665-bd70-fc07983dad15>
2010-05-12 10:51:22 +0000

    decision taken on 11.05.2010: use 160X100 design

--- js/globalnav.js
+++ js/globalnav.js
@@ -6,66 +6,68 @@
  * Date: 2010-04-29 18:07
  * Revision: 1
  */
-$(document).ready(function(){
+jQuery(document).ready(function(){
     /* Navigation: big panels */
-    var opened_panel = null;
-    function hide_panel(){
-        opened_panel.hide(); 
-        opened_panel = null;
+    function togglePanel(panel){
+        if( panel.is('.hidden') ){
+            jQuery("#header").expose({color: '#000', opacity: 0.6, loadSpeed: 0, closeSpeed: 0});
+        }
+        else{
+            jQuery.mask.close();
+        }
+        panel.toggleClass("hidden");
+        
+        
+        //show curtain
     }
-    function show_panel(panel){
-        if (opened_panel != null) opened_panel.hide();   
-        panel.show();
-        opened_panel = panel;
-    }
-    $('#main_menus .menu').mouseleave(function(){ hide_panel(); });
-    $('#public_menu_link').mouseenter(function(){ show_panel($('#public_panel')); });
-    $('#school_menu_link').mouseenter(function(){ show_panel($('#school_panel')); });
-    $('#brief_menu_link').mouseenter(function(){ show_panel($('#brief_panel')); });
-    
+    jQuery('#main_menus .menu').each(function(){
+        var panel = jQuery(this).children('.navigation_panel');
+        jQuery(this).mouseleave(function() { panel.addClass("hidden"); jQuery.mask.close()});
+        jQuery(this).click(function(){ togglePanel(panel); return true; });
+        jQuery(this).children('.main-link').click(function(){ togglePanel(panel); return false; });
+    });
     
     /* Navigation: search boxes */ 
-    var current_search_base = $('#search_form label.current');
+    var current_search_base = jQuery('#search_form label.current');
     function change_search_base(label){
         switch (label.attr('for')){
             case "search_engine_person":
-                $('#search_form').attr('action', 'http://search.epfl.ch/compoundDirectory.do');
-                $('#search_field').attr('name', 'q');
+                jQuery('#search_form').attr('action', 'http://search.epfl.ch/compoundDirectory.do');
+                jQuery('#search_field').attr('name', 'q');
                 break;
             case "search_engine_place":
-                $('#search_form').attr('action', 'http://plan.epfl.ch/');
-                $('#search_field').attr('name', 'room');
+                jQuery('#search_form').attr('action', 'http://plan.epfl.ch/');
+                jQuery('#search_field').attr('name', 'room');
                 break;
             case "search_engine_epfl":
-                $('#search_form').attr('action', 'http://search.epfl.ch/web.do');
-                $('#search_field').attr('name', 'q');
+                jQuery('#search_form').attr('action', 'http://search.epfl.ch/web.do');
+                jQuery('#search_field').attr('name', 'q');
                 break;
             case "search_engine_local":
-                $('#search_form').attr('action', 'http://search.epfl.ch/web.do');
-                $('#search_field').attr('name', 'q');
+                jQuery('#search_form').attr('action', 'http://search.epfl.ch/web.do');
+                jQuery('#search_field').attr('name', 'q');
                 break;
         }
-        if ($('#search_field').val() === $('#search_form label.current').attr('title')) $('#search_field').val('');
-        if ($('#search_field').val() === '') $('#search_field').val(label.attr('title'));
+        if (jQuery('#search_field').val() === jQuery('#search_form label.current').attr('title')) jQuery('#search_field').val('');
+        if (jQuery('#search_field').val() === '') jQuery('#search_field').val(label.attr('title'));
         current_search_base.toggleClass('current');
         current_search_base = label;
         current_search_base.toggleClass('current')
     }
+    change_search_base(jQuery("#search-box label[for='search_engine_person']"));
+    jQuery('#searchlink').click(function(){ jQuery('#search_field').focus();});
+    jQuery('#search_form label').click(function(){ change_search_base(jQuery(this)); });
+    jQuery('#search_field').focus(function(){
+        if (jQuery(this).val() === current_search_base.attr('title')) jQuery(this).val('').addClass('focused');
+    });
+    jQuery('#search_field').blur(function() {
+            if (jQuery(this).val() === '')  jQuery(this).val(jQuery('#search_form label.current').attr('title')).removeClass('focused');
+    });
+    jQuery('#search_field').keypress(function(e){
+            if (e.which == 13) jQuery(this).parent('form').submit();
+    });
+        
+    jQuery('.dropdown').click(function(){ jQuery(this).children('ul').toggle();});
+    jQuery('.dropdown').mouseleave(function(){ jQuery(this).children('ul').hide() });
     
-    $('#search_form label').click(function(){ change_search_base($(this)); });
-    $('#search_field').focus(function(){
-        
-        if ($(this).val() === current_search_base.attr('title')) $(this).val('').addClass('focused');
-        /*alert('here');*/
-    });
-    $('#search_field').blur(function() {
-            if ($(this).val() === '')  $(this).val($('#search_form label.current').attr('title')).removeClass('focused');
-    });
-    $('#search_field').keypress(function(e){
-            if (e.which == 13) $(this).parent('form').submit();
-        });
-        
-    $('.dropdown').click(function(){ $(this).children('ul').toggle();});
-    $('.dropdown').mouseleave(function(){ $(this).children('ul').hide() });
-
 });
