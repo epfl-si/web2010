8c368dd526281f1243eed668f2ae44556badf363
grfavre <grfavre@7c446e51-0b01-4665-bd70-fc07983dad15>
2010-05-21 12:59:55 +0000

    structure updates

--- js/globalnav.js
+++ js/globalnav.js
@@ -9,64 +9,54 @@
 jQuery(document).ready(function(){
     /* Navigation: big panels */
     function togglePanel(panel){
-        if( panel.is('.hidden') ){
-            jQuery("#header").expose({color: '#000', opacity: 0.6, loadSpeed: 0, closeSpeed: 0});
-        }
-        else{
-            jQuery.mask.close();
-        }
-        panel.toggleClass("hidden");
-        
-        
-        //show curtain
+        if (panel.is('.hidden')) jQuery("#header").expose({color: '#000', opacity: 0.6, loadSpeed: 0, closeSpeed: 0});
+        else jQuery.mask.close();
+        panel.toggleClass("hidden");   
     }
-    jQuery('#main_menus .menu').each(function(){
-        var panel = jQuery(this).children('.navigation_panel');
+    jQuery('#main-menus .menu').each(function(){
+        var panel = jQuery(this).children('.navigation-panel');
         jQuery(this).mouseleave(function() { panel.addClass("hidden"); jQuery.mask.close()});
         jQuery(this).click(function(){ togglePanel(panel); return true; });
         jQuery(this).children('.main-link').click(function(){ togglePanel(panel); return false; });
     });
     
     /* Navigation: search boxes */ 
-    var current_search_base = jQuery('#search_form label.current');
-    function change_search_base(label){
-        switch (label.attr('for')){
-            case "search_engine_person":
-                jQuery('#search_form').attr('action', 'http://search.epfl.ch/compoundDirectory.do');
-                jQuery('#search_field').attr('name', 'q');
+    var current_search_base = jQuery('#searchform label.current');
+    function change_search_base(radio){
+        rid = radio.attr('id')
+        label =  jQuery('label[for=' + rid + ']') 
+        switch (rid){
+            case "search-engine-person":
+                jQuery('#searchform').attr('action', 'http://search.epfl.ch/compoundDirectory.do');
+                jQuery('#searchfield').attr('name', 'q');
                 break;
-            case "search_engine_place":
-                jQuery('#search_form').attr('action', 'http://plan.epfl.ch/');
-                jQuery('#search_field').attr('name', 'room');
+            case "search-engine-place":
+                jQuery('#searchform').attr('action', 'http://plan.epfl.ch/');
+                jQuery('#searchfield').attr('name', 'room');
                 break;
-            case "search_engine_epfl":
-                jQuery('#search_form').attr('action', 'http://search.epfl.ch/web.do');
-                jQuery('#search_field').attr('name', 'q');
+            case "search-engine-epfl":
+                jQuery('#searchform').attr('action', 'http://search.epfl.ch/web.do');
+                jQuery('#searchfield').attr('name', 'q');
                 break;
-            case "search_engine_local":
-                jQuery('#search_form').attr('action', 'http://search.epfl.ch/web.do');
-                jQuery('#search_field').attr('name', 'q');
+            case "search-engine-local":
+                jQuery('#searchform').attr('action', 'http://search.epfl.ch/web.do');
+                jQuery('#searchfield').attr('name', 'q');
                 break;
         }
-        if (jQuery('#search_field').val() === jQuery('#search_form label.current').attr('title')) jQuery('#search_field').val('');
-        if (jQuery('#search_field').val() === '') jQuery('#search_field').val(label.attr('title'));
+        if (jQuery('#searchfield').val() === jQuery('#searchform label.current').attr('title')) jQuery('#searchfield').val('');
+        if (jQuery('#searchfield').val() === '') jQuery('#searchfield').val(label.attr('title'));
         current_search_base.toggleClass('current');
         current_search_base = label;
         current_search_base.toggleClass('current')
     }
-    change_search_base(jQuery("#search-box label[for='search_engine_person']"));
-    jQuery('#searchlink').click(function(){ jQuery('#search_field').focus();});
-    jQuery('#search_form label').click(function(){ change_search_base(jQuery(this)); });
-    jQuery('#search_field').focus(function(){
-        if (jQuery(this).val() === current_search_base.attr('title')) jQuery(this).val('').addClass('focused');
-    });
-    jQuery('#search_field').blur(function() {
-            if (jQuery(this).val() === '')  jQuery(this).val(jQuery('#search_form label.current').attr('title')).removeClass('focused');
-    });
-    jQuery('#search_field').keypress(function(e){
-            if (e.which == 13) jQuery(this).parent('form').submit();
-    });
-        
+    change_search_base(jQuery("#search-engine-person"));
+    jQuery('#searchlink').click(function(){ jQuery('#searchfield').focus();});
+    jQuery('#search-box input[type=radio]').change(function(){ change_search_base(jQuery(this)); });
+    jQuery('#searchfield').focus(function(){ if (jQuery(this).val() === current_search_base.attr('title')) jQuery(this).val('').addClass('focused'); });
+    jQuery('#searchfield').blur(function() { if (jQuery(this).val() === '')  jQuery(this).val(jQuery('#searchform label.current').attr('title')).removeClass('focused'); });
+    jQuery('#searchfield').keypress(function(e){ if (e.which == 13) jQuery(this).parent('form').submit(); });
+    
+    /* navigation: Dropdown menus */
     jQuery('.dropdown').click(function(){ jQuery(this).children('ul').toggle();});
     jQuery('.dropdown').mouseleave(function(){ jQuery(this).children('ul').hide() });
     
