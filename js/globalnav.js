6680c3fc935efc58c82f770c883461cf886a9562
grfavre <grfavre@7c446e51-0b01-4665-bd70-fc07983dad15>
2010-05-27 20:21:38 +0000

    fixed header/IE compatibility

--- js/globalnav.js
+++ js/globalnav.js
@@ -3,10 +3,10 @@
  *
  * Copyright (c) 2010 EPFL
  *
- * Date: 2010-04-29 18:07
- * Revision: 1
+ * Date: 2010-05-25 15:00
+ * Revision: 1.1
  */
-jQuery(document).ready(function(){
+jQuery(document).ready(function(){    
     /* Navigation: big panels */
     function togglePanel(panel){
         if (panel.is('.hidden')) jQuery("#header").expose({color: '#000', opacity: 0.6, loadSpeed: 0, closeSpeed: 0});
@@ -19,7 +19,6 @@ jQuery(document).ready(function(){
         jQuery(this).click(function(){ togglePanel(panel); return true; });
         jQuery(this).children('.main-link').click(function(){ togglePanel(panel); return false; });
     });
-    
     /* Navigation: search boxes */ 
     var current_search_base = jQuery('#searchform label.current');
     function change_search_base(radio){
@@ -47,11 +46,16 @@ jQuery(document).ready(function(){
         if (jQuery('#searchfield').val() === '') jQuery('#searchfield').val(label.attr('title'));
         current_search_base.toggleClass('current');
         current_search_base = label;
-        current_search_base.toggleClass('current')
+        current_search_base.toggleClass('current');
+        radio.blur();
+        radio.focus()
     }
     change_search_base(jQuery("#search-engine-person"));
-    jQuery('#searchlink').click(function(){ jQuery('#searchfield').focus();});
+    if (jQuery.browser.msie) {
+        jQuery('#search-box input[type=radio]').click(function(){change_search_base(jQuery(this)); this.blur(); this.focus()});
+    }
     jQuery('#search-box input[type=radio]').change(function(){ change_search_base(jQuery(this)); });
+    jQuery('#searchlink').click(function(){ jQuery('#searchfield').focus();});
     jQuery('#searchfield').focus(function(){ if (jQuery(this).val() === current_search_base.attr('title')) jQuery(this).val('').addClass('focused'); });
     jQuery('#searchfield').blur(function() { if (jQuery(this).val() === '')  jQuery(this).val(jQuery('#searchform label.current').attr('title')).removeClass('focused'); });
     jQuery('#searchfield').keypress(function(e){ if (e.which == 13) jQuery(this).parent('form').submit(); });
@@ -59,5 +63,9 @@ jQuery(document).ready(function(){
     /* navigation: Dropdown menus */
     jQuery('.dropdown').click(function(){ jQuery(this).children('ul').toggleClass('hidden');});
     jQuery('.dropdown').mouseleave(function(){ jQuery(this).children('ul').addClass('hidden') });
-    
+    jQuery('#main-navigation .dropdown').hoverIntent(
+        function(){ jQuery(this).children('ul').removeClass('hidden'); }, 
+        function(){ jQuery(this).children('ul').addClass('hidden');});
+    /*jQuery('#main-navigation .dropdown').hoverintent(jQuery(this).children('ul').toggleClass('hidden'), jQuery(this).children('ul').toggleClass('hidden'));*/
+    jQuery('.dropdown').click(function(){ return true;});
 });
