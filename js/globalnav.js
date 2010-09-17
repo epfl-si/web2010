3c3daa2c7fecd1af6a8a59200487ac084681dbae
charmier <charmier@7c446e51-0b01-4665-bd70-fc07983dad15>
2010-09-17 13:38:08 +0000

    replaced google analytics script

--- js/globalnav.js
+++ js/globalnav.js
@@ -114,16 +114,6 @@ jQuery(document).ready(function(){
     } 
    
     /* Google Analytics */
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-4833294-1']);
-    _gaq.push(['_setDomainName', '.epfl.ch']);
-    _gaq.push(['_setAllowHash', false]);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+    jQuery.jGoogleAnalytics('UA-4833294-1', {topLevelDomain: '.epfl.ch'} );
     
 });
