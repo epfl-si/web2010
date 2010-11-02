4778138bac1c98e75e8f350592d9bb2d4226be59
charmier <charmier@7c446e51-0b01-4665-bd70-fc07983dad15>
2010-11-02 13:58:03 +0000

    Suppression du systeme de cache

--- tools/label.php
+++ tools/label.php
@@ -48,44 +48,35 @@
 		$sr = 8; // default value
 	}
 
-	$uid = './graphics/label_'.md5($label.$s.$fg.$bg).$st.$sb.$sl.$sr.'.png';
-
 	header('Expires: '.gmdate('r', time()+31104000));
 	header('Last-Modified: Sat, 3 Mar 2001 12:34:56 GMT');
 	header('Cache-Control: max-age=31104000'); // one year
 
 	header('Content-type: image/png');
 
-	if ( file_exists($uid) ) {
-		@readfile($uid);
-		exit;
-	} else {
-		$fontpath = dirname($SCRIPT_FILENAME).'/fonts/Tgb_____.ttf';
+	$fontpath = dirname($SCRIPT_FILENAME).'/fonts/Tgb_____.ttf';
 
-		$size = @imagettfbbox($s,0,$fontpath,$label);
-		$dx = (int) abs($size[2] - $size[0]) + 1;
-		$dy = (int) abs($size[5] - $size[3]) + 1;
+	$size = @imagettfbbox($s,0,$fontpath,$label);
+	$dx = (int) abs($size[2] - $size[0]) + 1;
+	$dy = (int) abs($size[5] - $size[3]) + 1;
 
-		$im = @imagecreate ($dx + $sl + $sr, $dy + $st + $sb);
+	$im = @imagecreate ($dx + $sl + $sr, $dy + $st + $sb);
 
-		$brc = (int) hexdec($bg{0}.$bg{1});
-		$bgc = (int) hexdec($bg{2}.$bg{3});
-		$bbc = (int) hexdec($bg{4}.$bg{5});
+	$brc = (int) hexdec($bg{0}.$bg{1});
+	$bgc = (int) hexdec($bg{2}.$bg{3});
+	$bbc = (int) hexdec($bg{4}.$bg{5});
 
-		$frc = (int) hexdec($fg{0}.$fg{1});
-		$fgc = (int) hexdec($fg{2}.$fg{3});
-		$fbc = (int) hexdec($fg{4}.$fg{5});
+	$frc = (int) hexdec($fg{0}.$fg{1});
+	$fgc = (int) hexdec($fg{2}.$fg{3});
+	$fbc = (int) hexdec($fg{4}.$fg{5});
 
-		imagecolorallocate($im,$brc,$bgc,$bbc);
-		$txt = imagecolorallocate($im, $frc,$fgc,$fbc);
+	imagecolorallocate($im,$brc,$bgc,$bbc);
+	$txt = imagecolorallocate($im, $frc,$fgc,$fbc);
 
-		@imagettftext($im, $s, 0, $sl, $dy + $st - 1, $txt, $fontpath, $label);
+	@imagettftext($im, $s, 0, $sl, $dy + $st - 1, $txt, $fontpath, $label);
 
-		/* save image to cache */
-		@imagepng($im, $uid);
-		/* output image */
-		imagepng($im);
+	/* output image */
+	imagepng($im);
 
-		imagedestroy($im);
-	}
+	imagedestroy($im);
 ?>
