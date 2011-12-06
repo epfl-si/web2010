71dd7cc84eb57eabe4133eb6786521ea8af07432
grfavre <grfavre@7c446e51-0b01-4665-bd70-fc07983dad15>
2011-12-06 09:33:34 +0000

    fixed anchors

--- js/globalnav.js
+++ js/globalnav.js
@@ -29,7 +29,9 @@ function change_search_base(radio){
     current_search_base.toggleClass('current');
     radio.attr('checked','checked');
     radio.blur();
-    radio.focus();
+    if (document.referrer.indexOf('#') != -1) {
+        radio.focus();
+    }
 }
 
 function showPanel() {
@@ -134,6 +136,36 @@ jQuery(document).ready(function(){
     });
     jQuery("img[rel]").overlay();
     
+    if (jQuery('#calendar-nav').length) {
+        jQuery.tools.dateinput.localize("fr",  {
+            months:      'Janvier,F&eacute;vrier,Mars,Avril,Mai,Juin,Juillet,Ao&ucirc;t,' +
+                         'Septembre,Octobre,Novembre,D&eacute;cembre',
+            shortMonths: 'jan,f&eacute;v,mar,avr,mai,jun,jul,ao&ucirc;,sep,oct,nov,d&eacute;c',
+            days:        'dimanche,lundi,mardi,mercredi,jeudi,vendredi,samedi',
+            shortDays:   'Di,Lu,Ma,Me,Je,Ve,Sa'
+        });
+
+        jQuery.tools.dateinput.localize("en", {
+           months: 'January,February,March,April,May,June,July,August,September,October,November,December',
+           shortMonths:  'Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec',
+           days:         'Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday',
+           shortDays:    'Su,Mo,Tu,We,Th,Fr,Sa'
+        });
+
+        jQuery('<input type="text" />').dateinput({
+            firstDay: 1,
+            lang: jQuery('html').attr('lang'),
+            onHide: function() { return false; },
+            change: function() {
+          		window.location = 'http://memento.epfl.ch/cgi-bin/memento2010/select?memento=EPFL&amp;period_ATDATE=1&amp;date=' + this.getValue('yyyy-mm-dd');
+        	}                    
+        }).data("dateinput").show();
+
+        jQuery('#calroot').detach().appendTo(jQuery('#calendar-nav')).css({
+            position:'relative'
+        });        
+    }
+
     /* Google Analytics */
     jQuery.jGoogleAnalytics('UA-4833294-1', {topLevelDomain: '.epfl.ch'} );
     
