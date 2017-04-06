0ac25841f1cd0721754ea41d9fb3649918ca65a6
William Belle <william.belle@gmail.com>
2017-04-06 12:45:55 +0200

    add grunt task to minify css (with cookie consent)

---
+++ grunt/cssmin.js
@@ -0,0 +1,14 @@
+module.exports = {
+  release: {
+    files: {
+      'css/epfl.css': [
+        'src/css/epfl.css',
+        'bower_components/cookieconsent/build/cookieconsent.min.css'
+      ],
+      'css/cdh.css': [
+        'src/css/cdh.css',
+        'bower_components/cookieconsent/build/cookieconsent.min.css'
+      ]
+    }
+  }
+};
