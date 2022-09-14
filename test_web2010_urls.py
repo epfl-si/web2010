55d695428099c54c89c67c87cab009ee60a2a63b
Olivier Bieler <olivier.bieler@epfl.ch>
2022-09-14 10:45:57 +0200

    Add script to test web2010 urls (routing)

---
+++ test_web2010_urls.py
@@ -0,0 +1,45 @@
+import requests
+import uuid
+
+BASE_URL = 'https://www.epfl.ch'
+
+paths = {
+  '/accessibility.en.shtml': 200,
+  '/accessibility.fr.shtml': 200,
+  '/cgi-bin/csoldap/browse?unit=10339': 302,
+  '/cgi-bin/csoldap?sciper=199918': 302,
+  '/cgi-bin/csoldap/simple?sciper=263877': 302,
+  '/cssss': 404,
+  '/css/print.css': 200,
+  '/css/epfl.css': 200,
+  '/images/epfl_logo.gif': 200,
+  '/images/EPFL-logo.gif': 404,
+  '/img/arrow-more.png': 200,
+  '/javascript-help.en.shtml': 200,
+  '/javascript-help.fr.shtml': 200,
+  '/js/epfl-cookie-consent.js': 200,
+  '/js/globalnav.js': 200,
+  '/navigate.en.shtml': 200,
+  '/navigate.fr.shtml': 200,
+  '/templates/fragments/header.fr.html': 200,
+  '/templates/fragments/header.en.html': 200,
+  '/tools/label.php?s=22&BG=99cccc&FG=ffffff&label=Drebin': 302,
+}
+
+sum_test_passed = 0
+sum_test_failed = 0
+
+for path, code in paths.items():
+  url = BASE_URL + path + '?' + str(uuid.uuid4())
+  print(url)
+  response_head = requests.head(url)
+  response_code = response_head.status_code
+  if (response_code != code):
+    print('FAILED: {} -> {}, should be {}'.format(url, response_code, code))
+    sum_test_failed += 1
+  else:
+    sum_test_passed += 1
+
+print('\nTEST finished:')
+print('  {} passed.'.format(sum_test_passed))
+print('  {} failed.'.format(sum_test_failed))
