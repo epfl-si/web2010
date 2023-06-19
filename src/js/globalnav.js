86d9d57a3930caeeb862df82ba48ee0c90da9a33
Olivier Bieler <olivier.bieler@epfl.ch>
2023-06-19 10:35:04 +0200

    Update Google Analytics (UA → GA4) (#109)

--- src/js/globalnav.js
+++ src/js/globalnav.js
@@ -8,17 +8,12 @@
  * Revision: 1.5
  */
 
- /*
-  * This should be in html as recommended by Google...
-  */
- (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
- (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
- m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
- })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
- ga('create', 'UA-4833294-1', 'auto', 'web2010');
- ga('web2010.set', 'anonymizeIp', true);
- ga('web2010.send', 'pageview');
+// Google Tag Manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PJGBG5R');
 
 function showPanel() {
   jQuery("#header").expose({
